b33b16d35c349d559ffdad09b07fa57f797f0f15
Jerry Alfredo <jerryalfs@gmail.com>
2026-08-14 12:16:33 +0700

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.go
+++ Data Structures & Algorithms/is-anagram/submission-0.go
@@ -1,12 +1,21 @@
 func isAnagram(s string, t string) bool {
-	runesS := []rune(s)
-	runesT := []rune(t)
-	sort.Slice(runesS, func(i, j int) bool { return runesS[i] < runesS[j] })
-	sort.Slice(runesT, func(i, j int) bool { return runesT[i] < runesT[j] })
-	sortedStringS := string(runesS)
-	sortedStringT := string(runesT)
-	if sortedStringS != sortedStringT {
+	// check the length same or not, if not it's not anagram
+	if len(s) != len(t) {
 		return false
+	}
+	// alphabet in golang basically is number
+    // a is an anchor, all alphabet will subtract with a
+    // value of subtraction will put into slot base on alphabet index
+    // if value subtraction s and t getting combined not equal to zero it will lead not anagram
+	var count [26]int
+	for i:=0; i < len(s); i++ {
+		count[s[i] - 'a']++
+		count[t[i] - 'a']--
+	}
+	for _,c := range count {
+		if c != 0 {
+			return false
+		}
 	}
 	return true
 }
